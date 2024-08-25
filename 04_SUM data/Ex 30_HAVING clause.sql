f10841d8ecfd0b3d6fcc4b805adf7a1254588627
LiFangling2002 <e1353132@u.nus.edu>
2024-08-25 12:11:40 +0800

    Update Ex 30_HAVING clause.sql

--- 04_SUM data/Ex 30_HAVING clause.sql
+++ 04_SUM data/Ex 30_HAVING clause.sql
@@ -6,3 +6,4 @@ SELECT 	COUNT(customers.first_name) AS 'number of customers',
 FROM customers
 GROUP BY state
 HAVING total_points > 3000;
+-- HAVING 子句用于在分组后的数据上进行过滤。它通常与 GROUP BY 子句一起使用，作用是对分组后的结果进行条件筛选
